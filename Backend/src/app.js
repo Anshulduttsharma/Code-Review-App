726a208f38cc8f495376d2531ce514303231c6d0
Anshul <anshuls26@gmail.com>
2025-02-19 13:19:37 +0530

    add controller and routes files with updated app file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const aiRoutes = require("./Routes/ai.routes")
 
 
 const app = express()
@@ -7,4 +8,5 @@ app.get("/", (req,res)=>{
     res.send("Hello Server!!")
 })
 
+app.use("/ai", aiRoutes)
 module.exports = app;
